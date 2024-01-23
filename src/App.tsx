df65529cb0004901ceb71436ad6afb3732448c67
alvin <alvin00216@163.com>
2024-01-24 00:44:53 +0800

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import logo from './assets/logo.png';
 interface AppProps {}
 
 const App: React.FC<AppProps> = (props) => {
-  return <div className='c-[#f00]'>App</div>;
+  return (
+    <>
+      <div className='flex pl-24 pt-24 items-center text-16'>
+        <img src={logo} className='w-60px text-center align-middle' />
+        <div className='pl-16 font-500 subpixel-antialiased'>
+          Lenovo Vantage
+          <div className='c-gray'>
+            <sub>Make life lucky and happy</sub>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
+import './index.css';
 import 'uno.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
